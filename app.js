b769acf325c9f76a68104f22c5474e7138a00fbe
Lazar Nenovski <riliano@live.com>
2018-12-07 13:14:18 +0100

    Added lobby skeleton and implemented lobby creation

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require("express");
 
 var port = process.argv[2];
 
+var lobby = require("./lobby");
+
 var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/client"));
@@ -11,11 +13,14 @@ app.use(express.static(__dirname + "/client"));
 var server = http.createServer(app);
 
 var numLobbys = 0;
+var allLobbys = [];
 
 app.get("/create", function (req, res) {
-	let newLobbyID = ++numLobbys;
+	allLobbys.push(new lobby(numLobbys));
+	let newLobbyID = numLobbys;
 	res.render("create.ejs", {id: newLobbyID} );
 	console.log("New lobby with id " + newLobbyID);
+	numLobbys++;
 });
 
 app.get("/", function (req, res) {
---
+++ lobby.js
@@ -0,0 +1,33 @@
+/* lobby.js - the backbone of the chess game */
+
+var lobby = function (myID)
+{
+	this.id = myID;
+	this.clients = [];
+	//white is always position 0, black is position 1
+	this.clientsSize = 1;
+//	this.maxClientSize = 150;//replace with default constatns (implement in the future)
+	this.tableWidth = 8; //replace with default constatns
+	this.tableHeight = 8;//replace with default constatns
+	this.table = [[]];//replace with default constatns
+	this.currentPlayerTurn = 0;
+};
+
+/* Adds the given socket as a new client and returns the new number of clients */
+/*
+lobby.prototype.addClient = function (newClientID)
+{
+	console.log("New client in lobby No: " + this.id);
+	//TODO error checking, implement maxClientSize
+
+	this.clients[this.clientsSize] = newClientID;
+	this.clientsSize++;
+	return this.clientsSize;
+};*/
+
+lobby.prototype.getLobbySize = function () {return this.clientsSize;};
+lobby.prototype.getID = function() {return this.id;};
+
+//lobby.prototype.executeMessage = function (senderID, message) {};
+
+module.exports = lobby;
