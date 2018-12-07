4a364fc59bf266f7ddad71175dc7a3221cfab93d
Lazar Nenovski <riliano@live.com>
2018-12-07 20:56:57 +0100

    Added new routes(more backbones) and websockets

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var url = require("url");
 var http = require("http");
 var express = require("express");
-
-var port = process.argv[2];
+var ws = require("ws");
 
 var lobby = require("./lobby");
+
+var port = process.argv[2];
 
 var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/client"));
 
 var server = http.createServer(app);
+const wss = new ws.Server({server});
 
 var numLobbys = 0;
 var allLobbys = [];
@@ -23,10 +25,36 @@ app.get("/create", function (req, res) {
 	numLobbys++;
 });
 
+app.get("/join/:id", function (req, res) {
+	var id = parseInt(req.params.id);
+	if (id != id) //stupid js nan check
+	{
+		res.render("invalid.ejs", {id: req.params.id});
+		return;
+	}
+	for (let i=0;i<numLobbys;i++)
+	{
+		if (id == allLobbys[i].getID())
+		{
+			console.log("User found a lobby");
+			res.render("lobby.ejs", {id: id});
+			return;
+		}
+	}
+
+	res.render("invalid.ejs", {id: req.params.id});
+
+//	res.render("join.ejs", {});
+});
+
 app.get("/", function (req, res) {
 	res.render("index.html");
 	console.log("Sending a response");
 });
 
+wss.on("connection", function (socket) {
+	console.log("new socket connection");
+	
+});
 
 server.listen(port, function() {console.log("Listening on: "+port);});
